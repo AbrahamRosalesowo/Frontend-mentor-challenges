8c13100a4fe0d967533175298b8bd745b26748bd
Abrahamrosalesowo <abrahamreznov@outlook.com>
2021-10-28 17:11:30 -0700

    first commit

---
+++ caca.js
@@ -0,0 +1,12 @@
+const AbrahamRosales = {
+    pronouns: "he" | "him",
+    code: ["Javascript", "Typescript"],
+    askMeAbout: ["web dev", "books", "hardware", "modern mexican culture"],
+    technologies: {
+        backend: ["NodeJS", "Express"],
+        frontend: ["VueJS", "React"],
+        databases: ["MongoDB", "MySQL"],
+        misc: ["heroku", "webpack", "API's"]
+    },
+    currentFocus: ["web applications", "English language", "Adult basic education"]
+};
